17924f6958a25e37b078699c2541c2dd764bdfc7
Dietrich Daroch <dietr1ch@acm.org>
2017-11-12 16:05:06 -0800

    set PATH

--- conf.d/00-env.fish
+++ conf.d/00-env.fish
@@ -1,3 +1,10 @@
 set -x ALTERNATE_EDITOR "nvim"
 set -x EDITOR "emacsclient -c -nw"
 set -x VISUAL "emacsclient -c"
+
+
+set PATH /bin
+set PATH /usr/{local/,}bin $PATH
+set PATH /usr/bin/{site,vendor,core}_perl $PATH
+
+set PATH $HOME/.bin $PATH
