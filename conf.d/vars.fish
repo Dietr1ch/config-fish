0dffefd688994996a11440ba83b012f1478f62aa
Dietrich Daroch <dietr1ch@acm.org>
2017-11-18 09:51:25 -0800

    Use upstream bobthefish theme

--- conf.d/vars.fish
+++ conf.d/vars.fish
@@ -1,10 +1,11 @@
 
 # Theme setup
+set -g theme_newline_cursor yes
+set -g theme_newline_cursor_arrow_glyph no
 set -g theme_git_worktree_support yes
 set -g theme_display_hg yes
 set -g theme_show_exit_status yes
 # set -g theme_color_scheme dark
-set -g theme_newline_cursor clean
 
 set -U FZF_FIND_FILE_COMMAND "command locate \$PWD | sed 's#'\$PWD/'##'"
 # set -U FZF_FIND_FILE_COMMAND "command pt -l ''"
