015034eb49b5a32dc7b4f58598ad415a773d1cdd
Dietrich Daroch <dietr1ch@acm.org>
2017-12-10 20:00:04 -0800

    Use done plugin and fix autosuggestion color

--- conf.d/vars.fish
+++ conf.d/vars.fish
@@ -6,7 +6,12 @@ set -g theme_git_worktree_support yes
 set -g theme_display_hg yes
 set -g theme_show_exit_status yes
 # set -g theme_color_scheme dark
+set -U fish_color_autosuggestion 666
 
+# Use locate for file and directory completion
 set -U FZF_FIND_FILE_COMMAND "command locate \$PWD | sed 's#'\$PWD/'##'"
 # set -U FZF_FIND_FILE_COMMAND "command pt -l ''"
 set -U FZF_CD_COMMAND "command locate \$PWD | sed 's#'\$PWD/'##' | sed 's#\(.*\)/[^/]*#\1#' | uniq"
+
+# Notify on long commands
+set -U __done_min_cmd_duration 10000
