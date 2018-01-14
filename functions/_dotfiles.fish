71f3f8c66f17b3359c548ec49ec3204aa762a53c
Dietrich Daroch <dietr1ch@acm.org>
2018-01-14 07:15:02 -0800

    Init: Allow specifying the github user on _dotfiles

--- functions/_dotfiles.fish
+++ functions/_dotfiles.fish
@@ -1,19 +1,25 @@
 
-
 function _dotfiles -d 'Get dotfiles git'
 	cd
+
+	set -l user $USER
+	if count $argv >/dev/null
+		set -l user $argv[1]
+	end
+
 	if not git rev-parse --show-toplevel >/dev/null ^/dev/null
+		echo "Getting dotfiles for $user (https://github.com/$user/dotfiles)"
 		git init
 		  and git config status.showUntrackedFiles no
-		  and git remote add origin-http https://github.com/$USER/dotfiles
+		  and git remote add origin-http https://github.com/$user/dotfiles
 		  and git fetch origin-http
 		  and git reset --hard origin-http/master
 		git remote rm origin-http
-		git remote add origin gh:$USER/dotfiles
+		git remote add origin gh:$user/dotfiles
 		  and git fetch origin
 		  and git branch --set-upstream-to=origin/master master
-		git remote add private gh:$USER/dotfiles-private
+		git remote add private gh:$user/dotfiles-private
 		  and git fetch private
-		  and git checkout -b $USER private/$USER
+		  and git checkout -b $user private/$user
 	end
 end
