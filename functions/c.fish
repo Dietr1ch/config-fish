c19848cb197d3d6bc21500d612981d947ee54253
Dietrich Daroch <dietr1ch@acm.org>
2018-06-24 23:38:48 -0700

    Show directory contents after cd on `c`

--- functions/c.fish
+++ functions/c.fish
@@ -1,13 +1,25 @@
 
-function c -d "cd to a possibly new directory"
+function c -d "Better cd"
 	if count $argv >/dev/null
 		cd $argv[1]
 	else
-    set -l git_root (git rev-parse --show-toplevel)
-    if test -z $git_root
-      cd
-    else
-      cd $git_root
-    end
+		set -l git_root (git rev-parse --show-toplevel ^/dev/null)
+		set -l hg_root (hg root ^/dev/null)
+		if test -n $git_root
+			cd $git_root
+			# if test "$PWD" = "$git_root"
+			# 	cd
+			# else
+			# 	cd $git_root
+			# end
+		else if test -n $hg_root
+			cd $hg_root
+		else
+			cd
+		end
+	end
+
+	if test $status -eq 0
+		ll --color=always | less -R -F
 	end
 end
