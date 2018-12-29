ce0f37ad99c1bcc514a4fdf886ff29b59a525fbe
Dietrich Daroch <dietr1ch@acm.org>
2018-12-29 08:44:01 -0800

    Add git abbr

---
+++ conf.d/abbr-git.fish
@@ -0,0 +1,42 @@
+abbr g 'git'
+
+abbr gcl 'git clone --recursive'
+abbr gCL 'git clone'
+
+abbr gs 'git status'
+abbr gss 'git status .'
+abbr gd 'git diff'
+abbr gdc 'git diff --cached'
+abbr gds 'git diff --stat'
+abbr gdw 'git diff --ignore-all-space'
+
+abbr ga 'git add'
+abbr grm 'git rm --cached'
+
+abbr gco 'git checkout'
+abbr gb 'git branch'
+
+abbr gst 'git stash'
+abbr gc 'git commit'
+
+abbr gbl 'git blame'
+abbr gcp 'git cherry-pick'
+
+abbr gf 'git fetch'
+abbr gfa 'git fetch --all'
+
+abbr gl 'git l'
+abbr gla 'git la'
+abbr gll 'git ll'
+abbr glla 'git lla'
+
+abbr gp 'git pull'
+abbr gpp 'git push'
+abbr gPP 'git push --force-with-lease'
+abbr gm 'git merge'
+abbr gr 'git rebase -i'
+abbr grc 'git rebase --continue'
+abbr gra 'git rebase --abort'
+
+abbr gR 'git remote'
+
--- conf.d/aliases.fish
+++ conf.d/aliases.fish
@@ -1,47 +1,45 @@
 
-alias _r="exec fish"
+alias _r "exec fish"
 if which pacaur >/dev/null ^/dev/null
-	abbr p="pacaur"
+	abbr p "pacaur"
 else if which pacman >/dev/null ^/dev/null
-	abbr p="pacman"
+	abbr p "pacman"
 end
 
 if which exa >/dev/null ^/dev/null
-	abbr l="exa -F --group-directories-first"
-	abbr ll="exa -lF --git --group-directories-first"
+	abbr l "exa -F --group-directories-first"
+	abbr ll "exa -lF --git --group-directories-first"
 else
-	abbr l="ls --group-directories-first"
-	abbr ll="ls -l --group-directories-first"
+	abbr l "ls --group-directories-first"
+	abbr ll "ls -l --group-directories-first"
 end
-abbr la="l -a"
-abbr lla="ll -a"
+abbr la "l -a"
+abbr lla "ll -a"
 
-abbr f="fd"
-abbr r="ranger"
-abbr rs="rsync"
+abbr f "fd"
+abbr r "ranger"
+abbr rs "rsync"
 
-abbr e="$EDITOR"
-abbr ee="$VISUAL"
-abbr em="emacs"
-abbr ec="emacsclient"
+abbr e "$EDITOR"
+abbr ee "$VISUAL"
+abbr em "emacs"
+abbr ec "emacsclient"
 
-abbr v="vim"
-abbr n="nvim"
+abbr v "vim"
+abbr n "nvim"
 
-abbr m="ncmpcpp"
+abbr m "ncmpcpp"
 
-abbr g="git"
+abbr o "xdg-open"
 
-abbr o="xdg-open"
+abbr λ "ghci"
 
-abbr λ="ghci"
+abbr py "python"
+abbr ipy "ipython"
 
-abbr py="python"
-abbr ipy="ipython"
+alias clipCopy "xclip -sel clip"
+alias clipPaste "xclip -o"
+alias clipLength "clipPaste | wc"
 
-alias clipCopy="xclip -sel clip"
-alias clipPaste="xclip -o"
-alias clipLength="clipPaste | wc"
-
-abbr sc="systemctl"
-abbr scu="systemctl  --user"
+abbr sc "systemctl"
+abbr scu "systemctl  --user"
