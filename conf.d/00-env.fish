55863a277b604c29ffb3aa91685377bc4b3a78af
Dietrich Daroch <dietr1ch@acm.org>
2019-01-01 07:45:13 -0800

    Set XDG paths

--- conf.d/00-env.fish
+++ conf.d/00-env.fish
@@ -9,20 +9,21 @@ function add_to_path
 	end
 end
 
-# System path
+# System
 set PATH /bin
 add_to_path /usr/{local/,}bin
 add_to_path /usr/bin/{site,vendor,core}_perl
-# Nixos
+# Nix
 add_to_path /etc/profiles/per-user/$USER/bin
 add_to_path /run/current-system/sw/{,s}bin
 add_to_path /nix/var/nix/profiles/default/bin
 add_to_path /run/wrappers/bin
-# User path
+# User
 add_to_path $HOME/.nix-profile/bin
 add_to_path $HOME/.local/bin
 add_to_path $HOME/.cargo/bin
 add_to_path $HOME/.bin
+
 
 # Editor
 # ======
@@ -46,8 +47,27 @@ else if which vim >/dev/null ^/dev/null
 	set -x ALTERNATE_EDITOR "vim"
 end
 
+
 # Pager
 # =====
 set -x PAGER less
 set -x LESS "-iMSx4 -F"
 
+
+# XDG
+# ===
+# System
+#set -x XDG_RUNTIME_DIR /run/user/(id -u)
+#set -x XDG_DATA_DIRS /usr/share:/usr/local/share
+#set -x XDG_CONFIG_DIRS /etc/xdg
+
+# User
+set -x XDG_CACHE_HOME $HOME/.cache
+set -x XDG_CONFIG_HOME $HOME/.config
+set -x XDG_DATA_HOME $HOME/.local/share
+set -x XDG_DESKTOP_DIR $HOME/Desktop
+set -x XDG_DOWNLOAD_DIR $HOME/Downloads
+set -x XDG_DOCUMENTS_DIR $HOME/Documents
+set -x XDG_MUSIC_DIR $HOME/Music
+set -x XDG_PICTURES_DIR $HOME/Pictures
+set -x XDG_VIDEOS_DIR $HOME/Videos
