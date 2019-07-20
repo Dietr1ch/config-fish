019051fa6f21bfc109a1468e9ca50bd7ff2910c8
Dietrich Daroch <dietr1ch@acm.org>
2019-07-20 01:29:59 -0700

    Disable git worktree support
    bug: oh-my-fish/theme-bobthefish#196

--- conf.d/vars.fish
+++ conf.d/vars.fish
@@ -1,12 +1,16 @@
 
 # Theme setup
+set -g theme_show_exit_status yes
+
 set -g theme_newline_cursor clean
 set -g theme_newline_cursor_arrow_glyph no
-set -g theme_git_worktree_support yes
-set -g theme_display_hg yes
-set -g theme_show_exit_status yes
-# set -g theme_color_scheme dark
+#set -g theme_color_scheme dark
 set -U fish_color_autosuggestion 666
+
+# Disabled worktree (https://github.com/oh-my-fish/theme-bobthefish/issues/181)
+#set -g theme_git_worktree_support yes
+
+set -g theme_display_hg yes
 
 # Use locate for file and directory completion
 set -U FZF_FIND_FILE_COMMAND "command locate \$PWD | sed 's#'\$PWD/'##'"
