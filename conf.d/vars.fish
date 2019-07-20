01874a2477a81efa47a3b32b522393f1fb8321be
Dietrich Daroch <dietr1ch@acm.org>
2019-07-20 01:31:34 -0700

    Tweak prompt

--- conf.d/vars.fish
+++ conf.d/vars.fish
@@ -2,15 +2,24 @@
 # Theme setup
 set -g theme_show_exit_status yes
 
+set -g theme_powerline_fonts yes
 set -g theme_newline_cursor clean
 set -g theme_newline_cursor_arrow_glyph no
 #set -g theme_color_scheme dark
 set -U fish_color_autosuggestion 666
 
+set -g theme_display_git yes
+set -g theme_display_git_dirty yes
+set -g theme_display_git_master_branch no
 # Disabled worktree (https://github.com/oh-my-fish/theme-bobthefish/issues/181)
 #set -g theme_git_worktree_support yes
 
 set -g theme_display_hg yes
+
+set -g theme_display_hostname ssh
+set -g theme_display_user ssh
+
+set -g theme_display_vi yes
 
 # Use locate for file and directory completion
 set -U FZF_FIND_FILE_COMMAND "command locate \$PWD | sed 's#'\$PWD/'##'"
